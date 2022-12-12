dbf964eeb58490c85afd940244c09e5ae086c8c8
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-12 18:48:54 +0800

    Update NetworkJaal.py

--- NetworkJaal.py
+++ NetworkJaal.py
@@ -1,5 +1,5 @@
-#20221209 - streamlit with network visualization
-#Ref: https://medium.com/towards-data-science/how-to-deploy-interactive-pyvis-network-graphs-on-streamlit-6c401d4c99db
+#20221212 - streamlit with network visualization using Dash
+#Ref: https://community.plotly.com/t/dash-networkx-digraph-highlight-descendants-and-ancestors-paths-on-existing-graph/68660
 
 #import required library
 import streamlit as st
@@ -61,12 +61,87 @@ for edge in G.edges():
     end = G.nodes[edge[1]]['pos']
     edge_x, edge_y = addEdge(start, end, edge_x, edge_y, .8, 'end', .04, 30, nodeSize)
     
-
 edge_trace = go.Scatter(x=edge_x, y=edge_y, line=dict(width=lineWidth, color=lineColor), hoverinfo='none', mode='lines', text=text)
-
-
 node_trace = go.Scatter(x=node_x, y=node_y, text=text,mode='markers+text', hoverinfo='text', marker=dict(showscale=False, color = nodeColor, size=nodeSize))
 
+fig = go.Figure(data=[edge_trace, node_trace],
+             layout=go.Layout(
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                )
+             
+# Note: if you don't use fixed ratio axes, the arrows won't be symmetrical
+fig.update_layout(yaxis = dict(scaleanchor = "x", scaleratio = 1), plot_bgcolor='rgb(255,255,255)')
+    
+app = dash.Dash()
+app.layout = html.Div([
+    html.Div([
+        html.H1(children='Hello Dash'),
 
+        html.Div(children='''
+        Directed Water and Orange Juice Graph.
+         '''),
+        html.Div(  children=[
+
+            dcc.Dropdown([{'label': 'Connected', 'value': 'con'},{'label': 'Ascending', 'value': 'asc'},{'label': 'Descending', 'value': 'desc'}], '', id='typ-dropdown'),
+            html.Div(id='dd-typ-container'),
+            html.Div(html.Br()),
+            dcc.Dropdown([{'label': 'Node A', 'value': 0},{'label': 'Node B', 'value': 1},{'label': 'Node C', 'value': 2},{'label': 'Node D', 'value': 3},{'label': 'Node A1', 'value': 4},{'label': 'Node B1', 'value': 5},{'label': 'Node C1', 'value': 6}], '', id='node-dropdown'),
+            html.Div(id='dd-out-container')
+            
+            ]),
+
+        html.Div([
+            html.H3('Water Network'),
+
+            dcc.Graph(id='example-graph1', figure=fig),
+                        # dcc.Graph(id='example-graph', figure=fig)
+
+        ], className="six columns"),
+
+        html.Div([
+            
+
+            # dcc.Graph(id='example-graph2', figure=fig)
+        ], className="six columns"),
+    ], className="row")
+])
+
+
+# @app.callback(
+#     Output('dd-out-container', 'children'),
+#     Input('fig-dropdown', 'value')
+# )
+# def update_output(value):
+#     return f'you have selected {value}'
+
+@app.callback(
+    Output('example-graph1', 'children'),
+    Input('typ-dropdown', 'value'),
+    Input('node-dropdown', 'value')
+)
+
+def connected_graph(contyp, value):
+    desc = nx.descendants(G,value)
+    anc = nx.ancestors(G,value)
+    if contyp == 'desc':
+        paths = [path for p in desc for path in nx.all_simple_paths(G,value,p)]
+    elif contyp == 'anc':
+        paths = [path for p in anc for path in nx.all_simple_paths(G,value,p)]
+    elif contyp == 'con_':
+        paths = [path for p in desc for path in nx.all_simple_paths(G,value,p)]
+        paths.extend([path for p in anc for path in nx.all_simple_paths(G,p,value)])
+
+    Q = nx.DiGraph()
+    for p in paths:
+        nx.add_path(Q,p)
+
+    return Q
+
+
+app.run_server(debug=True)
 
 #End of Script  
